a72370c78d1419fbcaa8784cca2ef64b00f1de5c
shrimay18 <shrimaysomani18@gmail.com>
2024-07-17 20:00:27 +0530

    updated

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,5 +1,78 @@
-import "./App.css";
+// import "./App.css";
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import { useSelector } from "react-redux";
+// import Profile from "./pages/Profile";
+// import Admin from "./pages/Admin";
+// import BookShow from "./pages/BookShow";
+// import SingleMovie from "./pages/SingleMovie";
+// import Partner from "./pages/Partner";
+// import Forget from "./pages/Forget.js";
+// import Reset from "./pages/Reset.js";
+
+// function App() {
+//   const { loading } = useSelector((state) => state.loader);
+
+//   return (
+//     <div className="App">
+//       {loading && (
+//         <div className="loader-container">
+//           {" "}
+//           <div className="loader"> </div>{" "}
+//         </div>
+//       )}
+//       <BrowserRouter>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//                 <ProtectedRoute>
+//                   <Home  />
+//                 <ProtectedRoute/>
+//             }
+//           />
+//           <Route
+//             path="/admin"
+//             element={
+//                 <ProtectedRoute>
+//                   <Admin />
+//                 </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             path="/profile"
+//             element={
+//                 <Profile />
+//             }
+//           />
+//           <Route
+//             path="/partner"
+//             element={
+//                 <ProtectedRoute>
+//                   <Partner />
+//                 </ProtectedRoute>
+//             }
+//           />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/register" element={<Register />} />
+//           <Route path='/forget' element={<Forget/>}/>
+//           <Route path='/reset' element={<Reset/>}/>
+//           <Route path="/movie/:id" element={<ProtectedRoute><SingleMovie/></ProtectedRoute>} />
+//           <Route path="/book-show/:id" element={<ProtectedRoute><BookShow/></ProtectedRoute>} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -11,8 +84,8 @@ import Admin from "./pages/Admin";
 import BookShow from "./pages/BookShow";
 import SingleMovie from "./pages/SingleMovie";
 import Partner from "./pages/Partner";
-import Forget from "./pages/Forget.js";
-import Reset from "./pages/Reset.js";
+import Forget from "./pages/Forget";
+import Reset from "./pages/Reset";
 
 function App() {
   const { loading } = useSelector((state) => state.loader);
@@ -21,8 +94,7 @@ function App() {
     <div className="App">
       {loading && (
         <div className="loader-container">
-          {" "}
-          <div className="loader"> </div>{" "}
+          <div className="loader"></div>
         </div>
       )}
       <BrowserRouter>
@@ -30,39 +102,55 @@ function App() {
           <Route
             path="/"
             element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/admin"
             element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/profile"
             element={
+              <ProtectedRoute>
                 <Profile />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/partner"
             element={
-                <ProtectedRoute>
-                  <Partner />
-                </ProtectedRoute>
+              <ProtectedRoute>
+                <Partner />
+              </ProtectedRoute>
             }
           />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path='/forget' element={<Forget/>}/>
-          <Route path='/reset' element={<Reset/>}/>
-          <Route path="/movie/:id" element={<ProtectedRoute><SingleMovie/></ProtectedRoute>} />
-          <Route path="/book-show/:id" element={<ProtectedRoute><BookShow/></ProtectedRoute>} />
+          <Route path="/forget" element={<Forget />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route
+            path="/movie/:id"
+            element={
+              <ProtectedRoute>
+                <SingleMovie />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/book-show/:id"
+            element={
+              <ProtectedRoute>
+                <BookShow />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
